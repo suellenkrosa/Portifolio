cab65f2014c889c7d23f53b30df872f00299435f
suellenkrosa <suellenkretzschmar.pt@gmail.com>
2026-03-07 18:56:39 +0000

    Add initial HTML structure for Euromilhões page

---
+++ HTML + JavaScript/Euromilhoes.html/funcoes.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Euromilhões</title>
+    <script type="text/javascript" src="funcoes.js"></script>
+    
+</head>
+<body>
+    <p>Carregue no botão para ganhar o Euromilhões.</p>
+    <button id="btn" onmouseover="displayOk();">OK</button>
+</body>
+</html>
